7ec1f817419d77a4374c73887b0c2d817e1ef20b
Artem Momatov <momatovv@list.ru>
2023-10-09 23:37:55 +0300

    Base created

--- SADA/App.xaml.cs
+++ SADA/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,56 @@ namespace SADA
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Services = ConfigureServices();
+
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets the current <see cref="App"/> instance in use
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+        /// </summary>
+        public IServiceProvider Services { get; }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var view = Services.GetService<View.Start.AuthorizationView>();
+            view.Show();
+        }
+
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            ConfigureOtherServices(services);
+            ConfigureView(services);
+
+            return services.BuildServiceProvider();
+        }
+
+        private static void ConfigureView(ServiceCollection services)
+        {
+            services.AddSingleton<View.Start.AuthorizationView>();
+            services.AddSingleton<ViewModel.Start.AuthorizationViewModel>();
+
+            services.AddSingleton(provider => new View.Start.AuthorizationView
+            {
+                DataContext = provider.GetRequiredService<ViewModel.Start.AuthorizationViewModel>()
+            });
+        }
+
+        private static void ConfigureOtherServices(ServiceCollection services)
+        {
+            services.AddSingleton<Func<Type, ObservableObject>>(serviceProvider => viewModelType =>
+              (ObservableObject)serviceProvider.GetRequiredService(viewModelType));
+        }
+
+        
     }
 }
---
+++ SADA/ViewModel/Start/AuthorizationViewModel.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SADA.ViewModel.Start
+{
+    public class AuthorizationViewModel: ObservableObject
+    {
+        #region Fields
+
+        private string _login;
+        private string _password;
+
+        #endregion
+
+        #region Properties
+
+        public string Login
+        {
+            get { return _login; }
+            set { SetProperty(ref _login, value); }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand LoginCommand { get; }
+
+        #endregion
+
+        #region Command implementations
+
+        private void _LoginCommand()
+        {
+            MessageBox.Show($"{Login} {Password}");
+        }
+
+        #endregion
+
+        public AuthorizationViewModel()
+        {
+            LoginCommand = new RelayCommand(_LoginCommand);
+        }
+
+    }
+}
