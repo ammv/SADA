f7c09565ffba23fa3b4525c3acca20b82d9d5d96
Artem Momatov <momatovv@list.ru>
2023-10-10 23:13:56 +0300

    Add NavigationService based on Page

---
+++ SADA/Services/NavigationService.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SADA.Services
+{
+    public class NavigationService : ObservableObject, INavigationService
+    {
+        private Page _currentView;
+        private readonly IServiceProvider _provider;
+
+        public Page CurrentView
+        {
+            get => _currentView;
+            private set
+            {
+                SetProperty(ref _currentView, value);
+            }
+        }
+
+        public NavigationService(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public void NavigateTo<T>() where T : Page
+        {
+            var view = _provider.GetService(typeof(T)) as Page;
+            CurrentView = view;
+        }
+    }
+}
