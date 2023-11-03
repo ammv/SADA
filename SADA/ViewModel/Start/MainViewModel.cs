fa2fa1edf6e43ebc759289a94d5321459cd32972
Artem Momatov <momatovv@list.ru>
2023-11-03 11:53:24 +0300

    ghg

--- SADA/ViewModel/Start/MainViewModel.cs
+++ SADA/ViewModel/Start/MainViewModel.cs
@@ -1,5 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using HandyControl.Controls;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace SADA.ViewModel.Start
 {
@@ -9,12 +14,19 @@ namespace SADA.ViewModel.Start
         #region Constructor
         public MainViewModel()
         {
-            _tabItemInfos.Add(new TabItemInfo( "Мурад черт",
-                App.Current.GetService<TestViewModel>()));
-            _tabItemInfos.Add(new TabItemInfo("Мурад черт 1",
-                App.Current.GetService<TestViewModel>()));
-            _tabItemInfos.Add(new TabItemInfo("Мурад черт 2",
-                App.Current.GetService<TestViewModel>()));
+            //_tabItemInfos.Add(new TabItemInfo("Мурад черт",
+            //    App.Current.GetService<TestViewModel>()));
+            //_tabItemInfos.Add(new TabItemInfo("Мурад черт 1",
+            //    App.Current.GetService<TestViewModel>()));
+            //_tabItemInfos.Add(new TabItemInfo("Мурад черт 2",
+            //    App.Current.GetService<TestViewModel>()));
+
+            _menuMap.Add("Главная", typeof(TestViewModel));
+            _menuMap.Add("Администрирование", typeof(TestViewModel));
+            _menuMap.Add("Контрагенты", typeof(TestViewModel));
+            _menuMap.Add("Продажи", typeof(TestViewModel));
+
+            OpenTabCommand = new RelayCommand<string>(_OpenTabCommand);
         }
 
         #endregion
@@ -22,6 +34,8 @@ namespace SADA.ViewModel.Start
         #region Fields
 
         private ObservableCollection<TabItemInfo> _tabItemInfos = new ObservableCollection<TabItemInfo>();
+        private int _selectedTabItemIndex;
+        private Dictionary<string, Type> _menuMap = new Dictionary<string, Type>();
 
         #endregion
 
@@ -33,14 +47,52 @@ namespace SADA.ViewModel.Start
             set => SetProperty(ref _tabItemInfos, value);
         }
 
+        public int SelectedTabItemIndex
+        {
+            get => _selectedTabItemIndex;
+            set => SetProperty(ref _selectedTabItemIndex, value);
+        }
+
         #endregion
 
         #region Commands
+
+        public RelayCommand<string> OpenTabCommand { get; }
 
 
         #endregion
 
         #region Commands implementations
+
+        private void _OpenTabCommand(string parameter)
+        {
+
+            //1. Понять, открыта ли уже текущая вкладка
+            int index = -1;
+            for (int i = 0; i < _tabItemInfos.Count; i++)
+            {
+                if (_tabItemInfos[i].Name == parameter)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index != -1)
+            {
+                SelectedTabItemIndex = index;
+            }
+            else
+            {
+                if (_menuMap.TryGetValue(parameter, out Type viewModelType))
+                {
+                    _tabItemInfos.Add(new TabItemInfo(parameter,
+                    App.Current.Services.GetService(_menuMap[parameter]) as ObservableObject));
+                }
+
+            }
+
+        }
 
         #endregion
 
@@ -52,9 +104,11 @@ namespace SADA.ViewModel.Start
             {
                 Name = name;
                 ViewModel = viewModel;
+                GUID = Guid.NewGuid();
             }
 
             public string Name { get; set; }
+            public Guid GUID { get; }
             public ObservableObject ViewModel { get; set; }
 
             public override string ToString()
